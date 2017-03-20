63784b98e1d4eb927ec76aa955910abf997a5732
Robert Fridzema <robertfridzema@Roberts-MacBook-Pro.local>
2017-03-20 21:48:07 +0100

    .

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+var swiperContainer = document.getElementById('swiper-container');
+
+var mySwiper = new Swiper (swiperContainer, {
+	// Optional parameters
+	// direction: 'vertical',
+	// loop: true,
+
+	pagination: '.swiper-pagination'
+});
+
+console.log('App ready!');
